67826858662fa58e8162952c242472ae91e6e8b6
khh-Niloy <niloy.dev.101@gmail.com>
2025-06-23 18:19:13 +0600

    initial setup and add navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+  const navItems = [
+    { item: "Home", link: "/" },
+    { item: "Products", link: "/products" },
+    { item: "About", link: "#" },
+    { item: "Contact ", link: "#" },
+    { item: "Previous work", link: "#" },
+  ];
+  const pathname = usePathname();
+
+  const currentStatusColorStyle = {
+    active:
+      "text-white font-extralight bg-purple-700 underline rounded lg:bg-transparent lg:text-white lg:p-0",
+    inactive:
+      "text-white border-b border-gray-100 hover:bg-gray-50 font-extralight lg:hover:bg-transparent lg:border-0 lg:p-0",
+  };
+
+  const renderedNavItems = navItems.map(({ item, link }) => (
+    <li key={item}>
+      <a
+        href={link}
+        className={`text-[16px] hover:duration-300 font-extralight hover:font-semibold hover:transition-all block py-2 pl-3 pr-4 ${
+          pathname === link
+            ? currentStatusColorStyle.active
+            : currentStatusColorStyle.inactive
+        }`}
+        aria-current="page"
+      >
+        {item}
+      </a>
+    </li>
+  ));
+
+  return (
+    <div className="navbar bg-[#3661DF] text-white shadow-sm px-10 mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow "
+          >
+            {renderedNavItems}
+          </ul>
+        </div>
+        <a className="text-xl font-medium">Techno Cool Associates</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-10 text-white">
+          {renderedNavItems}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn hidden">Button</a>
+      </div>
+    </div>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -18,10 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html data-theme="light" lang="en">
+      <body className={`${inter.variable} antialiased bg-white`}>
+        <Navbar />
         {children}
       </body>
     </html>
